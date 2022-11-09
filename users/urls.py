196de5d04781ff16690b4bd5955c6dd57714cc51
WebCoder2021 <82173844+WebCoder2021@users.noreply.github.com>
2022-11-10 00:07:35 +0500

    dfgfdg

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('login', log_in, name='login'),
-    path('check_user', check_user, name='check_user'),
+    path('', log_in,name='login'),
+    path('logout/', logout_view,name='logout'),
+    path('check_user/', check_user,name='check_user'),
+    path('settings/', settings, name="settings"),
+    path('profile/', profile, name="profile"),
     
 ]

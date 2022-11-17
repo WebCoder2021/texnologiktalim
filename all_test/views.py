a9d86251adc2859488f18a0f2b300db54baa8f1d
WebCoder2021 <82173844+WebCoder2021@users.noreply.github.com>
2022-11-17 10:31:33 +0500

    dfgdfg

--- all_test/views.py
+++ all_test/views.py
@@ -3,22 +3,35 @@ from .models import *
 # Create your views here.
 def test(request):
     context = {}
-    context['tests'] = TestQuestion.objects.all()
-    context['old_results'] = UserTestResult.objects.filter(user=request.user)
-    if request.method == 'GET':
-        start = request.GET.get('start',False)
-        if start:
-            context['start'] = True
-    if request.method == "POST":
-        result = UserTestResult.objects.create(user=request.user)
-        for test in TestQuestion.objects.all():
-            print(test.id,request.POST.get(str(test.id),False))
-            ts = UserTest.objects.create(question_id=int(test.id),answer_id=int(request.POST.get(str(test.id),False))) 
-            ts.save()
-            
-            result.tests.add(ts)
-            result.save()
-            context['result'] = result
+    if request.user.is_authenticated:
+        context['tests'] = TestQuestion.objects.all().order_by('?')[:5]
+        tests = context['tests']
+        context['old_results'] = UserTestResult.objects.filter(user=request.user)
+        if request.method == 'GET':
+            start = request.GET.get('start',False)
+            if start:
+                context['start'] = True
+        if request.method == "POST":
+            result = UserTestResult.objects.create(user=request.user)
+            for p,value in request.POST.items():
+                if p != "csrfmiddlewaretoken":
+                    ts = UserTest.objects.create(question_id=int(p),answer_id=int(value)) 
+                    ts.save()
+                    result.tests.add(ts)
+                    result.save()
+                    context['result'] = result
+
+
+
+
+            # result = UserTestResult.objects.create(user=request.user)
+            # for test in tests:
+            #     ts = UserTest.objects.create(question_id=int(test.id),answer_id=int(request.POST.get(str(test.id),False))) 
+            #     ts.save()
+
+                # result.tests.add(ts)
+                # result.save()
+                # context['result'] = result
             
 
     return render(request,'test/test.html',context)
